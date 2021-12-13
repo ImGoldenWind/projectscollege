b8aac0dffc63a7721f2e28fc01deb2df2a03b424
ImGoldenWind <66796221+ImGoldenWind@users.noreply.github.com>
2021-12-13 11:08:47 +0300

    Add files via upload

---
+++ Calc_K-D-main/marafon_by_Aliev/marafon_by_Aliev/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace marafon_by_Gulov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Rasschitat = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rost = new System.Windows.Forms.TextBox();
+            this.ves = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.otvet = new System.Windows.Forms.Label();
+            this.otvetik = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Рост:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Вес:";
+            // 
+            // Rasschitat
+            // 
+            this.Rasschitat.Location = new System.Drawing.Point(66, 358);
+            this.Rasschitat.Name = "Rasschitat";
+            this.Rasschitat.Size = new System.Drawing.Size(75, 23);
+            this.Rasschitat.TabIndex = 2;
+            this.Rasschitat.Text = "Рассчитать";
+            this.Rasschitat.UseVisualStyleBackColor = true;
+            this.Rasschitat.Click += new System.EventHandler(this.Rasschitat_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Отмена";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rost
+            // 
+            this.rost.Location = new System.Drawing.Point(115, 239);
+            this.rost.Name = "rost";
+            this.rost.Size = new System.Drawing.Size(100, 20);
+            this.rost.TabIndex = 4;
+            // 
+            // ves
+            // 
+            this.ves.Location = new System.Drawing.Point(115, 289);
+            this.ves.Name = "ves";
+            this.ves.Size = new System.Drawing.Size(100, 20);
+            this.ves.TabIndex = 5;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(333, 358);
+            this.trackBar1.Maximum = 80;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(239, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 40;
+            // 
+            // otvet
+            // 
+            this.otvet.AutoSize = true;
+            this.otvet.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.otvet.Location = new System.Drawing.Point(444, 331);
+            this.otvet.Name = "otvet";
+            this.otvet.Size = new System.Drawing.Size(0, 24);
+            this.otvet.TabIndex = 7;
+            // 
+            // otvetik
+            // 
+            this.otvetik.AutoSize = true;
+            this.otvetik.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.otvetik.Location = new System.Drawing.Point(406, 239);
+            this.otvetik.Name = "otvetik";
+            this.otvetik.Size = new System.Drawing.Size(0, 31);
+            this.otvetik.TabIndex = 11;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::marafon_by_Gulov.Properties.Resources.bmi_healthy_icon;
+            this.pictureBox4.Location = new System.Drawing.Point(356, 48);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(198, 176);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::marafon_by_Gulov.Properties.Resources.female_icon;
+            this.pictureBox3.Location = new System.Drawing.Point(188, 48);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 154);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::marafon_by_Gulov.Properties.Resources.male_icon;
+            this.pictureBox2.Location = new System.Drawing.Point(41, 48);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 154);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::marafon_by_Gulov.Properties.Resources.Снимок;
+            this.pictureBox1.Location = new System.Drawing.Point(333, 380);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(614, 450);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.otvetik);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.otvet);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.ves);
+            this.Controls.Add(this.rost);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Rasschitat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Rasschitat;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox rost;
+        private System.Windows.Forms.TextBox ves;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label otvet;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label otvetik;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
